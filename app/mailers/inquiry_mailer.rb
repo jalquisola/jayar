45633761c59ce46566fb6432a686232627193156
jalquisola <joseph.alquisola@gmail.com>
2016-02-22 01:54:44 +0800

    add email in the subject

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -2,7 +2,7 @@ class InquiryMailer < ApplicationMailer
 
   def send_email(inquiry_params)
     mail(to: ENV['RECIPIENT_EMAIL'],
-       subject: inquiry_params[:unit_type],
+       subject: "#{inquiry_params[:unit_type]} from #{inquiry_params[:email]}",
        from: inquiry_params[:email])
   end
 
