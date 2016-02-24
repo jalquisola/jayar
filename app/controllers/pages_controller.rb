8a4bca459bcdb41a871aa04e691fa83214d15ecc
jalquisola <joseph.alquisola@gmail.com>
2016-02-25 01:47:39 +0800

    additional pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,25 @@
 class PagesController < ApplicationController
+  before_action :set_unit_types, only: [:home, :how_to_reserve, :contact_us]
   def home
     @inquiry = Inquiry.new
     @desc = "SMDC Coast Residences is situated in Roxas Boulevard, Pasay City. 3.6km away from SM Mall of Asia. Pre-selling Condos. 0% Interest. Php25,000 Reservation Fee."
     @title = "Coast Residences, Roxas Boulevard, Pasay City"
 
-    @unit_types = [
-      "1-Bedroom Unit(24.09sqm)",
-      "1-Bedroom Unit w/ Balcony(26.55-30.91sqm)",
-      "1-Bedroom Unit w/ Den(28.35-28.36sqm)",
-      "Family Suite A w/ Balcony(26.12-30.98sqm)",
-      "Family Suite B w/ Balcony(26.00-26.38sqm)",
-      "2-Bedroom Family Suite B w/ Balcony(29.35-32.12sqm)",
-      "2-Bedroom End Unit w/ Balcony(66.96-68.55sqm)",
-    ]
     render :home, layout: false
   end
 
-  def thanks
+  def how_to_reserve
     @inquiry = Inquiry.new
+    render :how_to_reserve, layout: false
+  end
+
+  def contact_us
+    @inquiry = Inquiry.new
+    render :contact_us, layout: false
+  end
+
+  private
+  def set_unit_types
     @unit_types = [
       "1-Bedroom Unit(24.09sqm)",
       "1-Bedroom Unit w/ Balcony(26.55-30.91sqm)",
@@ -27,6 +29,5 @@ class PagesController < ApplicationController
       "2-Bedroom Family Suite B w/ Balcony(29.35-32.12sqm)",
       "2-Bedroom End Unit w/ Balcony(66.96-68.55sqm)",
     ]
-    render :thanks, layout: false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root "pages#home"
   get :thanks, to: 'pages#thanks'
   resources :inquiries, only: [:create]
+
+  get "terms-of-service", to: "pages#terms_of_service"
+  get "privacy-policy", to: "pages#privacy_policy"
+  get "about-smdc", to: "pages#about_smdc"
+  get "how-to-reserve", to: "pages#how_to_reserve"
 end
